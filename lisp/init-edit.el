fa597744a854d7d315f3e22309f046fc21347a22
Jin Xue <csjinxue@outlook.com>
2018-11-17 21:57:47 +0800

    Added some editing functionalities

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -9,6 +9,17 @@
               tab-width 4
               indent-tabs-mode nil)
 
+;; Delete selection if you insert
+(use-package delsel
+  :ensure nil
+  :hook (after-init . delete-selection-mode))
+
+;; Automatically reload files was modified by external program
+(use-package autorevert
+  :ensure nil
+  :diminish auto-revert-mode
+  :hook (after-init . global-auto-revert-mode))
+
 ;; Jump to things in Emacs tree-style
 (use-package avy
   :bind (("C-'" . avy-goto-char-2))
@@ -19,6 +30,10 @@
 (use-package avy-zap
   :bind (("M-z" . avy-zap-to-char-dwim)
          ("M-Z" . avy-zap-up-to-char-dwim)))
+
+;; An all-in-one comment command to rule them all
+(use-package comment-dwim-2
+  :bind ("M-;" . comment-dwim-2))
 
 ;; A comprehensive visual interface to diff & patch
 (use-package ediff
@@ -42,5 +57,21 @@
 (use-package undo-tree
   :diminish undo-tree-mode
   :hook (after-init . global-undo-tree-mode))
+
+;; Goto last change
+(use-package goto-chg
+  :bind ("C-," . goto-last-change))
+
+;; Move to the beginning/end of line or code
+(use-package mwim
+  :bind (("C-a" . mwim-beginning-of-code-or-line)
+         ("C-e" . mwim-end-of-code-or-line)))
+
+;; Hideshow
+(use-package hideshow
+  :ensure nil
+  :bind (:map hs-minor-mode-map
+              ("C-`" . hs-toggle-hiding))
+  :diminish hs-minor-mode)
 
 (provide 'init-edit)
