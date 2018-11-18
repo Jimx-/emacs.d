9cacaa04231552f5026144ffa2b37d51a131edfa
Jin Xue <csjinxue@outlook.com>
2018-11-18 22:11:56 +0800

    Added org-publish

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -12,6 +12,59 @@
   ;; Babel
   (setq org-confirm-babel-evaluate nil
         org-src-fontify-natively t
-        org-src-tab-acts-natively t))
+        org-src-tab-acts-natively t)
+
+  (defvar load-language-list '((emacs-lisp . t)
+                               (perl . t)
+                               (python . t)
+                               (ruby . t)
+                               (js . t)
+                               (css . t)
+                               (sass . t)
+                               (C . t)
+                               (java . t)
+                               (plantuml . t)))
+
+  (org-babel-do-load-languages 'org-babel-load-languages load-language-list)
+
+  ;; Preview HTML
+  (use-package org-preview-html
+    :diminish org-preview-html-mode)
+
+  ;; Publishing
+  (setq org-publish-project-alist
+        '(("org-jimx"
+           ;; Path to your org files.
+           :base-directory "~/org/blog"
+           :base-extension "org"
+
+           ;; Path to your Jekyll project.
+           :publishing-directory "~/projects/jekyll/"
+           :recursive t
+           :publishing-function org-html-publish-to-html
+           :headline-levels 4
+           :html-extension "html"
+           :body-only t ;; Only export section between <body> </body>
+           )
+
+
+          ("org-static-jimx"
+           :base-directory "~/org/blog/assets"
+           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+           :publishing-directory "~/projects/jekyll/assets"
+           :recursive t
+           :publishing-function org-publish-attachment)
+
+          ("jimx" :components ("org-jimx" "org-static-jimx")))))
+
+(defun org-custom-link-img-follow (path)
+  (org-open-file-with-emacs path))
+
+(defun org-custom-link-img-export (path desc format)
+  (cond
+   ((eq format 'html)
+    (format "<img src=\"%s\" alt=\"%s\"/>" (substring path 2) desc))))
+
+(add-hook 'org-load-hook (lambda () (org-add-link-type "img" 'org-custom-link-img-follow 'org-custom-link-img-export)) t)
 
 (provide 'init-org)
