e6f3996dcc5f9296f35679caa2c1b35c79591593
Jin Xue <csjinxue@outlook.com>
2018-12-05 00:23:15 +0800

    Added LSP for Elixir

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -118,4 +118,13 @@
   :commands lsp-racket-enable
   :hook (racket-mode . lsp-racket-enable))
 
+;; Elixir support
+(use-package lsp-elixir
+  :after elixir-mode
+  :load-path "~/sources/lsp-elixir"
+  :commands lsp-elixir-enable
+  :hook
+  ((elixir-mode . lsp-elixir-enable)
+   (elixir-mode . (lambda () (add-hook 'before-save-hook 'lsp-format-buffer)))))
+
 (provide 'init-lsp)
