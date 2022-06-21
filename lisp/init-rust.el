2e31b0dc697c484d1ba6828351e2e5ec6bc6190d
Jin Xue <csjinxue@outlook.com>
2022-06-21 14:17:09 +0800

    Fix rust

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,21 +1,5 @@
-(use-package rust-mode
-  :config
-  (setq rust-indent-method-chain t)
-
-  ;; This is necessary because both plugins are fighting for supremacy in
-  ;; `auto-mode-alist', so rustic-mode *must* load second. It only needs to
-  ;; happen once.
-  ;;
-  ;; rust-mode is still required for `racer'.
-  (defun +rust|init ()
-    "Switch to `rustic-mode', if it's available."
-    (when (require 'rustic nil t)
-      (rustic-mode)))
-  (add-hook 'rust-mode-hook #'+rust|init))
-
 (use-package rustic
   :if (> emacs-major-version 26)
-  :after rust-mode
   :init
   (setq rustic-rls-pkg 'lsp-mode)
   :config
@@ -23,7 +7,7 @@
         rustic-flycheck-setup-mode-line-p nil))
 
 (use-package cargo
-  :after rust-mode
+  :disabled
   :hook ((rust-mode rustic-mode) . cargo-minor-mode)
   :config
   (setq cargo-process--enable-rust-backtrace t))
